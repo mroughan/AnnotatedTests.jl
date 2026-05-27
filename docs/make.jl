1f029727523bcf25db6ebcdce3ae73fabd7603ff
Matthew Roughan <matthew.roughan@adelaide.edu.au>
2026-05-27 16:10:09 +0930

    added docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,29 @@
+using Pkg
+
+Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+Pkg.instantiate()
+
 using Documenter
 using AnnotatedTests
 
+DocMeta.setdocmeta!(IncCSV, :DocTestSetup, :(using AnnotatedTests); recursive=true)
+
 makedocs(
-    sitename = "AnnotatedTests.jl",
-    modules = [AnnotatedTests],
-    format = Documenter.HTML(disable_git = true, edit_link = nothing, repolink = nothing),
-    pages = ["Home" => "index.md"],
-    remotes = nothing,
+    modules=[AnnotatedTests],
+    authors="Matthew Roughan <matthew.roughan@adelaide.edu.au>",
+    sitename="AnnotatedTests.jl",
+    format=Documenter.HTML(
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://matthew.roughan@adelaide.edu.au.github.io/AnnotatedTests.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md", 
+    ],
+    remotes=nothing,
+    checkdocs=:exports,
+)
+
+deploydocs(;
+    repo="github.com/mroughan/AnnotatedTests.jl",
 )
